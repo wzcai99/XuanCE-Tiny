928d1c575d894e0a8272e7b42bd86abd90d5fdc1
caiwzh <wz_cai@seu.edu.cn>
2023-06-08 15:17:07 +0800

    update configs,examples

--- xuance/policy/gaussian.py
+++ xuance/policy/gaussian.py
@@ -64,3 +64,60 @@ class ActorCriticPolicy(nn.Module):
             outputs[key] = a_param[key]
         outputs['critic'] = v
         return outputs,a,v
+
+class SACCriticNet(nn.Module):
+    def __init__(self,
+                 state_dim:int,
+                 action_dim:int,
+                 initialize,
+                 device):
+        super(CriticNet,self).__init__()
+        self.device = device
+        self.model = nn.Sequential(*mlp_block(state_dim+action_dim,state_dim,nn.LeakyReLU,initialize,device)[0],
+                                   *mlp_block(state_dim,1,None,initialize,device)[0])
+        self.output_shape = {'critic':()}
+    def forward(self,x:torch.Tensor,a:torch.Tensor):
+        return self.model(torch.concat((x,a),dim=-1))[:,0]
+    
+class SACPolicy(nn.Module):
+    def __init__(self,
+                 action_space:gym.Space,
+                 representation:torch.nn.Module,
+                 initialize,
+                 device):
+        assert isinstance(action_space, gym.spaces.Box)
+        super(SACPolicy,self).__init__()
+        self.action_space = action_space
+        self.input_shape = self.representation.input_shape.copy()
+        self.output_shape = self.representation.output_shape.copy()
+        
+        self.representation = representation
+        self.actor = ActorNet(self.representation.output_shape['state'][0],
+                              self.action_space.shape[0],
+                              initialize,
+                              device)
+        
+        self.criticA_representation = copy.deepcopy(representation)
+        self.criticA = SACCriticNet(self.representation.output_shape['state'][0],
+                                    self.action_space.shape[0],
+                                    initialize,
+                                    device)
+        
+        self.criticB_representation = copy.deepcopy(representation)
+        self.criticB = SACCriticNet(self.representation.output_shape['state'][0],
+                                    self.action_space.shape[0],
+                                    initialize,
+                                    device)
+        
+        self.targetA_critic_representation = copy.deepcopy(representation)
+        self.targetB_critic_representation = copy.deepcopy(representation)
+        self.target_criticA = copy.deepcopy(self.criticA)
+        self.target_criticB = copy.deepcopy(self.criticB)
+        
+        for key,value in zip(self.actor.output_shape.keys(),self.actor.output_shape.values()):
+            self.output_shape[key] = value
+        self.output_shape['criticA'] = ()
+        self.output_shape['criticB'] = ()
+        self.actor_parameters = list(self.representation.parameters()) + list(self.actor.parameters())
+        self.critic_parameters = list(self.criticA_representation.parameters()) + list(self.criticA.parameters()) + list(self.criticB_representation.parameters()) + list(self.criticB.parameters())
+        
