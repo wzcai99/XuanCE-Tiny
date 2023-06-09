8745964c1b9e736992ddde6cfdf97d89ba828bc9
TneitaP <zzmpassion@gmail.com>
2023-06-09 16:36:38 +0800

    [vcl7002] update dmc_ppo on both test and train (run)

--- example_win/test_dmc_ppo.py
+++ example_win/test_dmc_ppo.py
@@ -1,3 +1,4 @@
+import os 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -18,50 +19,58 @@ from xuance.agent import PPO_Agent
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--device",type=str,default="cuda:0")
+    
     parser.add_argument("--config",type=str,default="config/ppo/")
     parser.add_argument("--domain",type=str,default="walkerStand0608") # default: same config.yaml for env from the same domain
     parser.add_argument("--task_id",type=str,default="walker") # walker, swimmer, ...
     parser.add_argument("--env_id",type=str,default="stand") # stand, walk, ...
     parser.add_argument("--time_limit",type=int,default=100)
-    parser.add_argument("--pretrain_weight",type=str,default=None)
+    
+    parser.add_argument("--pretrain_weight",type=str,default=r"D:\zzm_codes\xuance_TneitapSimHand\models\walkerStand0608\ppo-79811\model-Fri Jun  9 15_37_31 2023-51000.pth")
+
     parser.add_argument("--render",type=bool,default=True)
+
     args = parser.parse_known_args()[0]
     return args
 
-def set_seed(seed):
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    random.seed(seed)
+to_cpu = lambda tensor: tensor.detach().cpu().numpy()
 
 if __name__ == "__main__":
+
     args = get_args()
     device = args.device
     config = get_config(args.config,args.domain)
-    set_seed(config.seed)
-   
-    # in some cases, the training environment is different with the testing environment
-    def build_train_envs(): 
-        env = dmc.DMControl(args.task_id,args.env_id, args.time_limit)
-        envs = [BasicWrapper(env) for _ in range(config.nenvs)]
-        return ObservationNorm(config, RewardNorm(config, ActionNorm(DummyVecEnv(envs)), train=True), train=True)
 
     def build_test_envs(): 
         env = dmc.DMControl(args.task_id,args.env_id, args.time_limit)
         envs = [BasicWrapper(env) for _ in range(2)]
-        return ObservationNorm(config, RewardNorm(config, ActionNorm(DummyVecEnv(envs)), train=False), train=False)
-
-    train_envs = build_train_envs()
-    mlp_hiddens = tuple(map(int, config.mlp_hiddens.split(",")))
-    representation = MLP(space2shape(train_envs.observation_space),mlp_hiddens,nn.LeakyReLU,nn.init.orthogonal_,device)
-    policy = Gaussian_ActorCritic(train_envs.action_space,representation,nn.init.orthogonal_,device)
-    if args.pretrain_weight:
-        policy.load_state_dict(torch.load(args.pretrain_weight,map_location=device))
-    optimizer = torch.optim.Adam(policy.parameters(),config.lr_rate)
-    scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.5,total_iters=config.train_steps/config.nsize * config.nepoch * config.nminibatch)
-    learner = PPO_Learner(config,policy,optimizer,scheduler,device)
-    agent = PPO_Agent(config,train_envs,policy,learner)
+        envs = RewardNorm(config, ActionNorm(DummyVecEnv(envs)), train=False)
+        envs.load_rms() # os.path.join(self.save_dir,"reward_stat.npy")
+        envs = ObservationNorm(config, envs, train=False)
+        envs.load_rms()
+        return envs
     
+    envs = build_test_envs()
 
-    agent.benchmark(build_test_envs(),config.train_steps,config.evaluate_steps,render=args.render)
+    mlp_hiddens = tuple(map(int, config.mlp_hiddens.split(",")))
+    representation = MLP(space2shape(envs.observation_space),mlp_hiddens,nn.LeakyReLU,nn.init.orthogonal_,device)
+    policy = Gaussian_ActorCritic(envs.action_space,representation,nn.init.orthogonal_,device)
+    policy.load_state_dict(torch.load(args.pretrain_weight,map_location=device))
+    print("use weights: ", args.pretrain_weight)
+    policy.eval()
+
+    obs,infos = envs.reset() # (nenvs, 24)
+
+    test_episode = 1000
+    current_episode = 0
+    while current_episode < test_episode:
+        print("[%03d]"%(current_episode))
+        envs.render("human")
+        # obs_Tsor = torch.from_numpy(obs['observation']).float().to(policy.actor.device)
+        _,act_Distrib,_ = policy.forward(obs) # (nenvs, 6)
+        act_Tsor = act_Distrib.sample()
+        next_obs,rewards,terminals,trunctions,infos = envs.step(to_cpu(act_Tsor))
+        for i in range(envs.num_envs):
+            if terminals[i] == True or trunctions[i] == True: 
+                current_episode += 1
+        obs = next_obs
